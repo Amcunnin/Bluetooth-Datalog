79f0ce018273a52b73dc5dd0fec6261932d1647e
ulab <ulab@128.153.134.186>
2016-11-29 13:51:49 -0500

    Initial Project

---
+++ Bluetooth Datalog/src/edu/clarkson/cunningham/edisontechnologies/Communicator.java
@@ -0,0 +1,5 @@
+package edu.clarkson.cunningham.edisontechnologies;
+
+public class Communicator {
+
+}
---
+++ Bluetooth Datalog/src/edu/clarkson/cunningham/edisontechnologies/Main.java
@@ -0,0 +1,10 @@
+package edu.clarkson.cunningham.edisontechnologies;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
